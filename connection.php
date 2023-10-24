fc710f421ede8421fef44ee379c25243d2e94a30
QuochuygrwCT <141642684+QuochuygrwCT@users.noreply.github.com>
2023-10-24 08:47:01 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost','root','','online_shopping')
+$conn = mysqli_connect('	y0ab0pt4uz2gsgikt','	brug5g0lughcah1m','	nuy2aewiwhdnpg5w','	uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com')
  or die("Can not connect database".mysqli_connect_error());
 ?>
