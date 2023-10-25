0c93264844f1475726345a4d690dafda8dd01ce1
QuochuygrwCT <141642684+QuochuygrwCT@users.noreply.github.com>
2023-10-25 08:14:32 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('	uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','	o7a4aoc268c2muud','egnbeusoe38rj8rn','	d67go0i15892v1k2')
+$conn = mysqli_connect('uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','	o7a4aoc268c2muud','egnbeusoe38rj8rn','	d67go0i15892v1k2')
  or die("Can not connect database".mysqli_connect_error());
 ?>
